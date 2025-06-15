54f55139be6d02fe320a2462328673ecfb1ff824
Babxxh <dmitry.babich@puaro.io>
2025-06-15 07:30:32 +0300

    first commit

---
+++ src/features/grade-list/GradeBox.svelte
@@ -0,0 +1,9 @@
+<script>
+	import AddForm from './ui/AddForm.svelte'
+	import GradeList from './ui/GradeList.svelte'
+</script>
+
+<div>
+	<AddForm />
+	<GradeList />
+</div>
---
+++ src/features/header/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { ArrowRight } from '@lucide/svelte'
+	import { Button } from '$shared/ui/kit/button'
+</script>
+
+<header class="sticky top-2 bg-(--background)/10 backdrop-blur-lg">
+	<div
+		class="flex justify-between py-2.5 px-[40px] items-center max-w-[820px] w-full mx-auto border-b-1 border-b-white/5 border-t-1 border-t-white/5"
+	>
+		<div class="leading-none">Grade ILYA.</div>
+		<Button>
+			Send
+			<ArrowRight />
+		</Button>
+	</div>
+</header>
