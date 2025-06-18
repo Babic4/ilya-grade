ee24d56e515a3b1081810010609d29aa688c8053
Babxxh <dmitry.babich@puaro.io>
2025-06-18 21:11:24 +0300

    update scroll top

--- src/features/reviews/ui/ReviewList.svelte
+++ src/features/reviews/ui/ReviewList.svelte
@@ -1,20 +1,68 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte'
+	import { onDestroy, onMount } from 'svelte'
+	import { Button } from '$shared/ui/kit/button'
+	import { ArrowUp } from '@lucide/svelte'
 	import ReviewItem from './ReviewItem.svelte'
 	import { useReviews } from '../api/firebase'
 
+	let stickyRef: HTMLElement
+	let isStuck = false
+
 	const { reviews, unsubscribe } = useReviews()
+
+	const pullUp = () => {
+		const headerReviews = document.querySelector('div[data-up]')
+		const rectHeader = headerReviews?.getBoundingClientRect()
+		const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+		const topPosition = rectHeader?.top ?? 0
+
+		window.scrollTo({
+			top: topPosition + scrollTop - 120,
+			behavior: 'smooth',
+		})
+	}
+
+	onMount(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				// если элемент больше не пересекает родительскую область видимости сверху
+				// console.log(entry.boundingClientRect.top, entry.isIntersecting)
+				isStuck = entry.boundingClientRect.top < 76 && !entry.isIntersecting
+			},
+			{
+				threshold: [1],
+				root: null,
+				rootMargin: '-73.6px 0px 0px 0px', // отслеживаем "прилипание"
+			}
+		)
+
+		console.log(stickyRef)
+		if (stickyRef) observer.observe(stickyRef)
+
+		return () => {
+			observer.disconnect()
+		}
+	})
 
 	onDestroy(() => {
 		unsubscribe()
 	})
 </script>
 
-<div class="flex flex-col gap-3">
+<div class="flex flex-col gap-3" data-up>
 	<div
-		class="sticky px-6 py-3 top-[73.6px] border-b border-b-(--border) z-20 bg-(--background)/10 backdrop-blur-lg rounded-t-xl"
+		bind:this={stickyRef}
+		class="sticky flex items-center justify-between px-6 py-3 top-[73.6px] border-b border-b-(--border) z-20 bg-(--background)/10 backdrop-blur-lg rounded-t-xl"
 	>
-		Reviews: {$reviews.length}
+		<span class="leading-[28px]">Reviews: {$reviews.length}</span>
+		{#if isStuck}
+			<Button
+				onclick={pullUp}
+				class="p-1.5! h-min transition-[border-radius] duration-250 ease-[cubic-bezier(0.65, 0, 0.35, 1)] hover:rounded-[0.70rem]"
+			>
+				<ArrowUp />
+			</Button>
+		{/if}
 	</div>
 	{#each $reviews as review}
 		<ReviewItem {review} />
