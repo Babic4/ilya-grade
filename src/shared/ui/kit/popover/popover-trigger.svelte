a34a7147763ca1b3f0941cfef6e4f32eaa4b867e
Babxxh <dmitry.babich@puaro.io>
2025-06-19 07:47:00 +0300

    remove outline

--- src/shared/ui/kit/popover/popover-trigger.svelte
+++ src/shared/ui/kit/popover/popover-trigger.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { cn } from "$shared/lib/css.js";
-	import { Popover as PopoverPrimitive } from "bits-ui";
+	import { cn } from '$shared/lib/css.js'
+	import { Popover as PopoverPrimitive } from 'bits-ui'
 
 	let {
 		ref = $bindable(null),
 		class: className,
 		...restProps
-	}: PopoverPrimitive.TriggerProps = $props();
+	}: PopoverPrimitive.TriggerProps = $props()
 </script>
 
 <PopoverPrimitive.Trigger
 	bind:ref
 	data-slot="popover-trigger"
-	class={cn("", className)}
+	class={cn('focus-visible:outline-none!', className)}
 	{...restProps}
 />
